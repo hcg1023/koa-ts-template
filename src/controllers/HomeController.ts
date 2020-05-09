18429409c5c58005be7961edf9ca1a78a3928b57
hcg1023 <2293885211@qq.com>
2020-05-09 22:08:29 +0800

    feat: 修复模板默认页面

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,7 +1,13 @@
 import { Context } from 'koa'
+import * as fs from 'fs'
+import config from '../config'
 
 export default class HomeController {
   static hellWord(ctx: Context) {
-    ctx.body = process.env.NODE_ENV + process.env.npm_package_name
+    fs.readFile(config.staticPath + 'index.html', (err, data) => {
+      if (!err) {
+        ctx.body = data
+      }
+    })
   }
 }
